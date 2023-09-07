761f4eff8aeb8abbaea8cd13c3903a93ed95906c
ValSyacy <novalsyacy11@gmail.com>
2023-09-07 09:45:43 +0700

    tes

--- Jobsheet1/Latihan02.java
+++
@@ -1,6 +0,0 @@
-public class Latihan02 {
-    public static void main(String[] args) {
-        System.out.print("Nama Saya Alfitroh Syacy Novalino");
-        //System.out.print("\nSaya Adalah Mahasiswa Baru Jurusan Teknologi Informasi-POLINEMA");
-    }
-}
